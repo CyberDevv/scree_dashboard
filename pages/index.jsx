d1898578124efea68257d0a329cf516c3d044404
CyberDevv <toCyberdevv@gmail.com>
2021-11-19 01:02:08 +0100

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import tw from 'twin.macro';
+
+export default function Home() {
+   return (
+      <div>
+         <Title>What the fuck</Title>
+      </div>
+   );
+}
+
+// Tailwind styles
+const Title = tw.h2`text-5xl text-blue-800`;
