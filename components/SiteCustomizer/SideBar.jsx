99f0b321d1de5e175578536bd8b07433a64c6099
CyberDevv <tocyberdevv@gmail.com>
2021-12-03 22:42:42 +0100

    Finished the site customizer for now

---
+++ components/SiteCustomizer/SideBar.jsx
@@ -0,0 +1,54 @@
+import tw from 'twin.macro';
+import { useState } from 'react';
+import { Button as AddSection } from '@mui/material';
+
+import AddSectionSVG from '../../public/svg/addSection.svg';
+
+const SideBar = () => {
+   const [active, setActive] = useState('Hero');
+   return (
+      <Nav>
+         <SectionText>Sections</SectionText>
+
+         <NavButtonsWrapper>
+            {buttons.map((button, index) => (
+               <Button
+                  css={[active === button && tw`bg-[#fafafa] text-textBg-dark`]}
+                  key={index}
+               >
+                  {button}
+               </Button>
+            ))}
+         </NavButtonsWrapper>
+         <AddSection
+            startIcon={<AddSectionSVG />}
+            variant='text'
+            fullWidth
+            sx={{
+               textTransform: 'capitalize',
+               color: '#525252',
+               fontSize: '16px',
+            }}
+         >
+            Add Section
+         </AddSection>
+      </Nav>
+   );
+};
+
+const buttons = [
+   'Hero',
+   'CTA',
+   'Arrivals',
+   'Features',
+   'Testimonials',
+   'Footer',
+];
+
+// Tailwind styles
+const Nav = tw.nav`min-w-[180px] w-[180px] bg-white text-center xl:(py-12)`;
+const SectionText = tw.h5``;
+const NavButtonsWrapper = tw.div`my-16 space-y-5`;
+const Button = tw.button`block text-center w-full py-3.5 text-textBg-light xl:(text-[20px] font-semibold)`;
+
+export default SideBar;
--- components/page_components/SiteCustomizer.jsx
+++ components/page_components/SiteCustomizer.jsx
@@ -1,9 +1,23 @@
-import NavBar from '../SiteCustomizer/NavBar.jsx'
+import tw from 'twin.macro';
+
+import NavBar from '../SiteCustomizer/NavBar.jsx';
+import SideBar from '../SiteCustomizer/SideBar.jsx';
 
 const SiteCustomizer = () => {
-  return <div>
-     <NavBar />
-   </div>;
+   return (
+      <>
+         <NavBar />
+
+         <Main>
+            <SideBar />
+
+            <Site></Site>
+         </Main>
+      </>
+   );
 };
+// Tailwind styles
+const Main = tw.div`flex space-x-8 mt-4`;
+const Site = tw.div`bg-primary-sky w-full h-[120vh]`;
 
 export default SiteCustomizer;
